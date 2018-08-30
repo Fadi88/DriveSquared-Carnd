5aad5f6f440eabdbff7eb6d08be64f164727b6a2
Fadi Sedik <fady88@gmail.com>
2018-08-30 07:57:16 -0700

    addding working models

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -10,7 +10,7 @@ class TLClassifier(object):
 		self.graph = tf.Graph()
 		with tf.Session(graph=self.graph) as sess:
 			od_graph_def = tf.GraphDef()
-			od_graph_def.ParseFromString(tf.gfile.GFile('light_classification/saved_model.pb', 'rb').read())
+			od_graph_def.ParseFromString(tf.gfile.GFile('light_classification/model_sim.pb', 'rb').read())
 			tf.import_graph_def(od_graph_def, name='')
 
 		self.input_tensor              = self.graph.get_tensor_by_name('image_tensor:0')
